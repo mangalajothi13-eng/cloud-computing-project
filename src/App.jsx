d7c757292bbec7b375f0ad1e757c235bf55693f7
mangalajothi13-eng <mangalajothi13@gmail.com>
2026-06-16 23:52:15 +0530

    feat: setup proper React component structure with App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Project is running successfully!</h1>
+      <p>Welcome to my Cloud Computing project.</p>
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from'./App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Project is running successfully!</h1>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
+  
